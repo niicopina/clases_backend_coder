0c6310cc8221557c6dacc3a93a0d1adb48d05e08
nicopina <pinanicolasagustin@gmail.com>
2023-04-11 20:52:10 -0300

    workshop

--- calculos.js
+++ calculos.js
@@ -4,13 +4,14 @@ function suma(n1, n2){
     return new Promise(
         (resolve, reject) => {
             let verificarN1 = esNumero(n1)
-            console.log(verificarN1)
 
             let verificarN2 = esNumero(n2)
-            console.log(verificarN2)
 
             if(verificarN1.number && verificarN2.number){
                 let verificarSumaMayorACero = verificarN1.number + verificarN2.number
+                if(verificarSumaMayorACero){
+                    return resolve(verificarSumaMayorACero)
+                }
             }else{
                 reject({
                     error1: verificarN1.message, 
@@ -29,6 +30,8 @@ function suma(n1, n2){
 /* suma(5,10)
 suma(0, 8) */
 suma('hola', 23).then(res=>console.log(res)).catch(err=>console.log(err))
+suma(2, 3).then(res=>console.log(res)).catch(err=>console.log(err))
+suma(0, 3).then(res=>console.log(res)).catch(err=>console.log(err))
 
 
 function esNumero(num){
@@ -51,4 +54,19 @@ async function calculos(num1,num2,operacion){
         console.log(error)
         return error
     }
+}
+function resta (n1, n2){
+    return new Promise(
+        (resolve, reject)=> {
+            let verificarN1 = esNumero(n1)
+            let verificarN2 = esNumero(n2)
+            if(verificarN1.number && verificarN2.number){
+                let resultado = verificarN1.number - verificarN2.number
+                return resolve(resultado)
+            }
+            if(resultaod>0){
+
+            }
+        }
+    )
 }
