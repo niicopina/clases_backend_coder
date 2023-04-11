71275a49e1fecdeccf52765f711b95322f134b76
nicopina <pinanicolasagustin@gmail.com>
2023-04-11 19:32:59 -0300

    catch err

--- index.js
+++ index.js
@@ -30,5 +30,15 @@ let dividBien = (num1, num2) => {
     }
     )   
 }
+let respuesta1 = dividBien(10, 3) //cuando tiene exito usaremos THEN
+    .then(respuesta => console.log(respuesta))
+    .catch(respuesta => console.log(respuesta))
+    console.log(respuesta1)
 
+let respuesta2 = dividBien(10, 0) //cuando se rechaza usaremos CATCH
+    .then(res => console.log(`el resultado es ${res}`))
+    .catch(err=> console.log(err))
+
+
+console.log(respuesta2)
 
