1837656ce4d465057c41be1fbc63ece8ed84112c
nicopina <pinanicolasagustin@gmail.com>
2023-04-11 19:21:59 -0300

    promesa

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-let nombre = "nico";
+/* let nombre = "nico";
 let edad = 27;
 let precio = 1231;
 const series = [
@@ -16,6 +16,19 @@ edad++;
 series.push("House of Cards");
 
 console.log(series);
-console.log(edad);
+console.log(edad); */
+
+let dividBien = (num1, num2) => {
+    return new Promise ((resolve, reject) => {
+        let resultado
+        if(num2===0){
+            resultado = reject('no se puede')
+        }else{
+            resultado = resolve(num1 / num2)
+        }
+        return resultado
+    }
+    )   
+}
 
 
