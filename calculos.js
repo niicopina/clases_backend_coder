2c59f589defe6a03dfcb7840d157f5d598083345
nicopina <pinanicolasagustin@gmail.com>
2023-04-12 20:26:18 -0300

    multiplicar con resultado unicamente positivo

--- calculos.js
+++ calculos.js
@@ -30,6 +30,7 @@ suma(0, 8) */
 suma('hola', 23).then(res=>console.log(res)).catch(err=>console.log(err))
 suma(2, 3).then(res=>console.log(res)).catch(err=>console.log(err))
 suma(0, 3).then(res=>console.log(res)).catch(err=>console.log(err))
+multiplicar(0, 3).then(res=>console.log(res)).catch(err=>console.log(err))
 
 
 function esNumero(num){
@@ -79,7 +80,23 @@ function resta (n1, n2){
 function multiplicar (n1, n2){
     return new Promise(
         (resolve, reject) => {
-
+            let verificarN1 = esNumero(n1)
+            let verificarN2 = esNumero(n2)
+            if(verificarN1.number && verificarN2.number){
+                let resultado = verificarN1.number * verificarN2.number
+                if(resultado > 0){
+                    return resolve(resultado)
+                }else{
+                    return reject({
+                        error: 'La calculadora solo debe devolvel valores positivos'
+                    })
+                }
+            }else{
+                return reject({
+                    n1: verificarN1.message ?? 'El número es correcto', 
+                    n2: verificarN2.message ?? 'El número es correcto'
+                })
+            }
         }
     )
 }
