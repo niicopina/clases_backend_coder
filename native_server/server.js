beab99c42bf46f73db07b7e293c123540bfb76d6
nicopina <pinanicolasagustin@gmail.com>
2023-04-20 20:33:57 -0300

    calculadora de cuántos días tenés de edad

--- native_server/server.js
+++ native_server/server.js
@@ -2,22 +2,20 @@ const http = require('http')     //requiero el modulo nativo de node para crear 
 const moment = require('moment') //requiero el modulo externo de moment para manejar fechas
 
 let currentDate = moment()
-let validCurrentDate = currentDate.isValid()
-console.log(validCurrentDate)
-currentDate = currentDate.format("MMM Do YY")
+let validDate = currentDate.isValid()
+validDate = currentDate.format("MMM Do YY")
 
 let bornDate = moment("19950615")
 let validBornDate = bornDate.isValid()
-bornDate = bornDate.format("MMM Do YY")
-console.log(bornDate)
+let formatBornDate = bornDate.format("MMM Do YY")
 
-let days
-
+let days = currentDate.diff(bornDate, 'days') / 1000
+console.log(days)
 const server = http.createServer( //creo un servidor
-    (req, res) => res.end('WELCOME TO MY NEW SERVER!!')  //defino la callback pa la 'vista' del servidor
+    (req, res) => res.end(`hoy tenes ${days} dias de edad`)  //defino la callback pa la 'vista' del servidor
 )
 
-const PORT = 8080               // defino Puerto donde se levantará el servidor
+const PORT = 8000               // defino Puerto donde se levantará el servidor
 const callback = () => console.log('server ready on PORT: ' + PORT) //defino la callback que se ejecuta cuando se levanta el servidor
 
 server.listen(
